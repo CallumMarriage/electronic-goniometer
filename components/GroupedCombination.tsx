ea4d43dc03c8286d66e3bfde8ec44949d4eeb5a6
CallumMarriage <callummarriage14@gmail.com>
2024-12-14 19:38:40 +0000

    limits

--- components/GroupedCombination.tsx
+++ components/GroupedCombination.tsx
@@ -10,9 +10,6 @@ export const GroupedCombination = (props) => {
 
     const [panPosition, setPanPosition] = useState({ x: 0, y: 0 })
 
-    const [centerPointX, setCenterPointX] = useState(0)
-    const [centerPointY, setCenterPointY] = useState(200)
-
     const [rotatedPointX, setRotatedPointX] = useState(0)
     const [rotatedPointY, setRotatedPointY] = useState(0)
 
@@ -23,6 +20,7 @@ export const GroupedCombination = (props) => {
     const [dimensions, setDimensions] = useState({
         window: window
     });
+
 
     useEffect(() => {
         const subscription = Dimensions.addEventListener(
@@ -58,30 +56,46 @@ export const GroupedCombination = (props) => {
 
     useEffect(() => {
         groupPan.addListener((value) => {
-            setPanPosition(value)
+            let coordinates = {x: panPosition.x, y: panPosition.y}
+            if(value.x >= 0 && value.x < dimensions.window.width) {
+                coordinates.x = value.x
+            } 
+            if(value.y >= 0 && value.y <= dimensions.window.height) {
+                coordinates.y = value.y
+            }
+            console.log(value)
+            console.log(panPosition.x + ':' + panPosition.y + ' - ' +coordinates.x + ':' + coordinates.y)
+            setPanPosition(coordinates)
         });
+
+        return () => {
+            groupPan.removeAllListeners();
+          };
     })
 
+
     return (
-        <View style={styles.container} id="container">
+        <View style={[styles.container]} id="container">
             <Animated.View
                 id="animated-view"
                 style={{
-                    transform: [{ translateX: Animated.diffClamp(groupPan.x, 0, window.width - 50) }, { translateY: Animated.diffClamp(groupPan.y, 0, window.height - 25) }],
-                    width: dimensions.window.width,
-                    height: dimensions.window.height,
-                    overflow: 'visible'
+                    transform: [{ translateX: Animated.diffClamp(groupPan.x, 0, window.width ) }, { translateY: Animated.diffClamp(groupPan.y, 0, window.height) }],
+                    // width: dimensions.window.width,
+                    // height: dimensions.window.height,
+                    overflow: 'visible',
+                    zIndex: 99
                 }}
                 {...groupPanResponder.panHandlers}>
                 <View>
                     <View style={[styles.circle, { transform: [{ translateX: fixedPointX }, { translateY: fixedPointY }], zIndex: 100 }]} />
-                    <View style={[styles.circle, { transform: [{ translateX: centerPointX }, { translateY: centerPointY }], zIndex: 100 }]} />
+                    <View style={[styles.circle, { transform: [{translateY: 200}], zIndex: 100 }]} />
 
                     <MoveablePoint
                         setX={setRotatedPointX}
                         setY={setRotatedPointY}
                         dot={
-                            <View style={styles.circle} />
+                            <View style={[styles.circle]}>
+                                                  </View>
                         }
                         window={dimensions.window}
                         x={rotatedPointX}
@@ -89,25 +103,25 @@ export const GroupedCombination = (props) => {
 
                         zIndex={100}
                     />
-                    <LineBetweenPoints
-                        source={{ x: centerPointX + 25, y: centerPointY + 75}}
-                        target={{ x: rotatedPointX + 25, y: rotatedPointY + 125 }}
-                        width={dimensions.window.width}
-                        height={dimensions.window.height}
-                    />
-
-                    <LineBetweenPoints
-                        source={{ x: centerPointX + 25, y: centerPointY + 100 }}
-                        target={{ x: fixedPointX + 25, y: fixedPointY }}
-                        width={dimensions.window.width}
-                        height={dimensions.window.height}
-                    />
                 </View>
             </Animated.View>
+            <LineBetweenPoints
+                source={{ x: panPosition.x + 25, y: panPosition.y + 200 + 75 }}
+                target={{ x: panPosition.x + rotatedPointX + 25, y: panPosition.y + rotatedPointY + 125 }}
+                width={dimensions.window.width}
+                height={dimensions.window.height}
+            />
 
+            <LineBetweenPoints
+                source={{ x: panPosition.x + 25, y: panPosition.y + 200 + 100 }}
+                target={{ x: panPosition.x + 25, y: panPosition.y }}
+                width={dimensions.window.width}
+                height={dimensions.window.height}
+            />
         </View>
     )
 }
+
 
 const styles = StyleSheet.create({
     container: {
@@ -122,6 +136,8 @@ const styles = StyleSheet.create({
         width: 50,
         backgroundColor: 'blue',
         borderRadius: 50,
+        justifyContent: 'center',
+        alignContent: 'center'
     },
     animatedView: {
         overflow: 'visible'
--- components/LineBetweenPoints.tsx
+++ components/LineBetweenPoints.tsx
@@ -1,15 +1,17 @@
-import { StyleSheet } from "react-native"
+import { useRef } from "react"
+import { Animated, StyleSheet } from "react-native"
 import Svg, { Line } from "react-native-svg"
 
 export const LineBetweenPoints = (props) => {
+
     const edgeSource = getEdges(props.source, props.target)
 
     const edgeTarget = getEdges(props.target, props.source)
 
-    return (
-        <Svg height={props.height} width={props.width} style={[StyleSheet.absoluteFill, styles.line, { zIndex: 1, overflow: 'visible' }]}>
-            <Line x1={edgeSource.x} y1={edgeSource.y} x2={edgeTarget.x} y2={edgeTarget.y} stroke="red" strokeWidth="6" />
-        </Svg>
+    return ( 
+            <Svg height={props.height} width={props.width} style={[StyleSheet.absoluteFill, styles.line, { zIndex: 1, overflow: 'visible' }]}>
+                <Line x1={edgeSource.x} y1={edgeSource.y} x2={edgeTarget.x} y2={edgeTarget.y} stroke="red" strokeWidth="6" />
+            </Svg>
     )
 }
 
@@ -26,7 +28,6 @@ const getEdges = (pointA: { x: number, y: number }, pointB: { x: number, y: numb
 
 const styles = StyleSheet.create({
     line: {
-        overflow: 'visible',
         zIndex: 1
     }
 })
